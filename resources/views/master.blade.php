872af790a2349499be6574baf942fc9dfb30386a
adhiwiradh <adhi.wiradh@gmail.com>
2024-01-22 15:49:02 +0700

    add vendor

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -7,12 +7,12 @@
     <title>MOM - Meval</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
-    <link rel="stylesheet" href="{{ asset('/css/app.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('/public/css/app.css') }}" type="text/css">
     
-    <script src="{{ asset('/js/multiselect-dropdown.js') }}" ></script>
+    <script src="{{ asset('/public/js/multiselect-dropdown.js') }}" ></script>
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet"/>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.ckeditor.com/ckeditor5/38.1.0/classic/ckeditor.js"></script>
@@ -57,7 +57,7 @@ $.ajaxSetup({
                 <hr>
                 <div class="dropdown pb-4">
                     <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src=" {{ asset('images/account-icon.png') }}" alt="Image" width="30" height="30">
+                        <img src=" {{ asset('/public/images/account-icon.png') }}" alt="Image" width="30" height="30">
                         <span class="d-none d-sm-inline mx-1">{{Auth::user()->name}}</span>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
